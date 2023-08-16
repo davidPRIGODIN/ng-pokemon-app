8585f8fb7aa29400c2eb07353edd8fbb234e98a0
davidPRIGODIN <79266421+davidPRIGODIN@users.noreply.github.com>
2023-08-16 10:04:20 +0200

    Create ng-pokemon-app

---
+++ src/app/mock-pokemon-list.ts
@@ -0,0 +1,31 @@
+import { Pokemon } from './pokemon'
+
+export const POKEMONS : Pokemon[] = [
+{
+   id : 1,
+   name : "Bulbizarre", 
+   hp : 25,
+   cp : 5,
+   picture : "",
+   types: ["Plante","Poison"],
+   created: new Date()
+},
+{
+    id : 2,
+    name : "Salamèche", 
+    hp : 28,
+    cp : 6,
+    picture : "",
+    types: ["Feu"],
+    created: new Date()
+ },
+ {
+    id : 3,
+    name : "Carapuce", 
+    hp : 21,
+    cp : 4,
+    picture : "",
+    types: ["Eau"],
+    created: new Date()
+ }
+]
