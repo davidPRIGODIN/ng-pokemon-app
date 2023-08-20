34074b977a60815e1178176167ab162659acc992
davidPRIGODIN <79266421+davidPRIGODIN@users.noreply.github.com>
2023-08-20 16:44:20 +0200

    Create PokemonService and PokemonFormComponent

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-page-not-found',
   template: `
   <div class='center'> 
-    <img/>
+    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/035.png"/>
     <h1>Hey, cette page n'existe pas!</h1>
     <a routerLink ="/pokemons" class="waves-effect waves-teal btn-flat">
       Retourner à l'accueil
