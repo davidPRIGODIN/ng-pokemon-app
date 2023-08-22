25fff88fba4046a7f90c3385a5a33d3e55340091
davidPRIGODIN <79266421+davidPRIGODIN@users.noreply.github.com>
2023-08-22 15:09:26 +0200

    Simulate database

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -51,7 +51,7 @@ export class PokemonFormComponent implements OnInit{
 
 
   onSubmit(){
-    console.log('Submit form !')
-    this.router.navigate(['/pokemon', this.pokemon.id])
+    this.pokemonService.updatePokemon(this.pokemon)
+      .subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]))
   }
 }
